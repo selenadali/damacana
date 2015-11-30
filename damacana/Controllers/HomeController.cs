b3dbb4fca944d6e630ec39c4df207cc5b4974d99
Selen ADALI <selenadali94@gmail.com>
2015-11-30 18:58:05 +0200

    product + list<Product>

--- damacana/Controllers/HomeController.cs
+++ damacana/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using damacana.Models;
+
 
 namespace damacana.Controllers
 {
@@ -10,7 +12,23 @@ namespace damacana.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            Product product1 = new Product()
+            {
+                Id = 1,
+                Name = "Erikli",
+                Price = (decimal)9.50,
+            };
+            Product product2 = new Product()
+            {
+                Id = 2,
+                Name = "Hayat",
+                Price = (decimal)7.90,
+            };
+
+            List<Product> products = new List<Product>();
+            products.Add(product1);
+            products.Add(product2);
+            return View(products);
         }
 
         public ActionResult About()
